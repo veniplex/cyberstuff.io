e320cf76db96765542a30c8c3b4b54f062552807
veniplex <19841573+veniplex@users.noreply.github.com>
2024-02-26 22:28:50 +0100

    added theme mode switch

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import("tailwindcss").Config} */
 export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	darkMode: "selector",
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				background: "var(--color-background)",
+				text: "var(--color-text)",
+				primary: "var(--color-primary)"
+			}
+		},
 	},
 	plugins: [
 		require("@tailwindcss/typography")
