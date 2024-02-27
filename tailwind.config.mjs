6156fed00b2c235dc15c7e9f8e3f914d73764a54
veniplex <19841573+veniplex@users.noreply.github.com>
2024-02-27 21:52:56 +0100

    fancified navbar

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,13 @@
 /** @type {import("tailwindcss").Config} */
 export default {
-	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-	darkMode: "selector",
-	theme: {
-		extend: {
-			colors: {
-				background: "var(--color-background)",
-				text: "var(--color-text)",
-				primary: "var(--color-primary)"
-			}
-		},
-	},
-	plugins: [
-		require("@tailwindcss/typography")
-	]
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "selector",
+  theme: {
+    extend: {
+      colors: {
+        primary: "var(--color-primary)"
+      }
+    }
+  },
+  plugins: [require("@tailwindcss/typography")]
+};
